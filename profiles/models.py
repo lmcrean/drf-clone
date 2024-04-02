dec60dd05d1db32e53cea6512ccba791b3f00d6a
Laurie Crean <lmcrean@gmail.com>
2024-04-02 12:11:22 +0000

    Add ProfileDetail APIView with get, put methods

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,7 @@
+
 from django.db import models
 from django.db.models.signals import post_save
-# Create your models here.
-from django. contrib.auth.models import User
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
@@ -24,5 +24,6 @@ class Profile(models.Model):
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(owner=instance)
+
 
 post_save.connect(create_profile, sender=User)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -13,3 +15,31 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        print('put method activated')
+        profile = self.get_object(pk)
+        print('profile:', profile)
+        serializer = ProfileSerializer(profile, data=request.data)
+        print('serializer:', serializer)
+        if serializer.is_valid():
+            serializer.save()
+            print('serializer saved')
+            return Response(serializer.data)
+        print('serializer not valid')
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
