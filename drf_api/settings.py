866ed3e9cffb9448eccf4e6abb434795cf474fff
Laurie Crean <lmcrean@gmail.com>
2024-04-11 09:31:35 +0000

    Enable debug mode in development environment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -34,7 +34,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+# DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
