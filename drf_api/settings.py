e34f2b9ae64404e9a34e4394ac8f41d9ae54e1c7
Laurie Crean <lmcrean@gmail.com>
2024-04-11 09:56:35 +0000

    Update DEBUG setting to use environment variable

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -34,8 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     'localhost',
