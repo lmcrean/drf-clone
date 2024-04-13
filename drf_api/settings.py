57b793b69ee2d0f38440b1ba5adf3da1b6caf63a
Laurie Crean <lmcrean@gmail.com>
2024-04-13 13:55:36 +0000

    update CLIENT_ORIGIN to accomodate gitpod

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -111,9 +111,10 @@ MIDDLEWARE = [
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
-     CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
-     ]
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re. IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+    rf"{extracted_url}(eu|us) \d+\w\.gitpod\.io$",
+    ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
         r"^https://.*\.gitpod\.io$",
