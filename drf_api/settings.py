4159cc27420dcc89ea5f57753400ee10a09fb9b0
Laurie Crean <lmcrean@gmail.com>
2024-04-20 14:29:33 +0000

    set debug to true for bug inspection with post not sending in React

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
